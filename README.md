df318186bf8e026eae8fdfd9fc9506de5a46d848
Matthew Barr <mbarr@snap-interactive.com>
2014-10-06 23:48:26 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 hiera_param
 ===========
 
-Hiera Parameter
+Assigns parameters to a node using an hash merge lookup that retrieves the
+values for a user-specified key from a Hiera data source.
+
+To use `hiera_param`, the following configuration is required:
+
+- A key name to use for parameters, e.g. `params`.
+- A line in the puppet `sites.pp` file (e.g. `/etc/puppet/manifests/sites.pp`)
+  reading `hiera_param('params')`. Note that this line must be outside any node
+  definition and below any top-scope variables in use for Hiera lookups.
+- Class keys in the appropriate data sources. In a data source keyed to a node's role,
+  one might have:
+
+  ---
+  params:
+    dc: 'NYC'
+    cluster: 'web'
+
+In addition to the required `key` argument, `hiera_param` accepts two additional
+arguments:
+- a `default` argument in the second position, providing an hash to be returned
+  in the absence of matches to the `key` argument
+- an `override` argument in the third position, providing a data source to consult
+  for matching values, even if it would not ordinarily be part of the matched hierarchy.
+  If Hiera doesn't find a matching key in the named override data source, it will continue
+  to search through the rest of the hierarchy.
---
+++ lib/puppet/parser/functions/hiera_param.rb
@@ -0,0 +1,44 @@
+require 'hiera_puppet'
+
+module Puppet::Parser::Functions
+  newfunction(:hiera_param, :arity => -2, :doc => "Assigns parameters to a node
+  using an hash merge lookup that retrieves the values for a user-specified key
+  from a Hiera data source.
+
+  To use `hiera_param`, the following configuration is required:
+
+  - A key name to use for parameters, e.g. `params`.
+  - A line in the puppet `sites.pp` file (e.g. `/etc/puppet/manifests/sites.pp`)
+    reading `hiera_param('params')`. Note that this line must be outside any node
+    definition and below any top-scope variables in use for Hiera lookups.
+  - Class keys in the appropriate data sources. In a data source keyed to a node's role,
+    one might have:
+
+            ---
+            params:
+              dc: 'NYC'
+              cluster: 'web'
+
+  In addition to the required `key` argument, `hiera_param` accepts two additional
+  arguments:
+  - a `default` argument in the second position, providing an hash to be returned
+    in the absence of matches to the `key` argument
+  - an `override` argument in the third position, providing a data source to consult
+    for matching values, even if it would not ordinarily be part of the matched hierarchy.
+    If Hiera doesn't find a matching key in the named override data source, it will continue
+    to search through the rest of the hierarchy.
+
+  ") do |*args|
+    key, default, override = HieraPuppet.parse_args(args)
+    if answer = HieraPuppet.lookup(key, default, self, override, :hash)
+      # method = Puppet::Parser::Functions.function(:include)
+      # send(method, [answer])
+      answer.each do |name,value| 
+        self[name] = value
+      end
+    else
+      raise Puppet::ParseError, "Could not find data item #{key}"
+    end
+  end
+end
+
